e69208b54ad44a9346ad89f8eb88b11b9a9cb064
Evan Doyle <evanmdoyle@gmail.com>
2023-06-10 18:42:02 +0200

    Fix HttpAgentHandle ConnectionClosedError

--- roster_agent_runtime/agents/http.py
+++ roster_agent_runtime/agents/http.py
@@ -17,11 +17,17 @@ class HttpAgentHandle(AgentHandle):
         # Any other logic here? validation?
         return cls(name=name, url=url)
 
-    async def _request(self, method: str, url: str, **kwargs) -> aiohttp.ClientResponse:
+    async def _request(
+        self, method: str, url: str, *, raise_for_status: bool = True, **kwargs
+    ) -> dict:
         try:
             async with aiohttp.ClientSession() as session:
                 async with session.request(method, url, **kwargs) as response:
-                    return response
+                    if raise_for_status:
+                        assert response.status == 200
+                    return await response.json()
+        except AssertionError as e:
+            raise errors.TaskError("Agent returned an error.") from e
         except aiohttp.ClientError as e:
             raise errors.TaskError(f"Could not connect to agent {self.name}.") from e
 
@@ -29,86 +35,54 @@ class HttpAgentHandle(AgentHandle):
         self, chat_history: list[ConversationMessage]
     ) -> ConversationMessage:
         try:
-            response = await self._request(
+            response_data = await self._request(
                 "POST",
                 f"{self.url}/chat",
                 json=[message.dict() for message in chat_history],
             )
-            assert response.status == 200
-            response_text = await response.text()
-            return ConversationMessage(message=response_text, sender=self.name)
-        except AssertionError as e:
-            raise errors.TaskError(f"Could not chat with agent {self.name}.") from e
-        except (pydantic.ValidationError, aiohttp.ContentTypeError) as e:
+            return ConversationMessage(**response_data)
+        except pydantic.ValidationError as e:
             raise errors.TaskError(
-                f"Could not parse response from agent {self.name}."
+                f"Could not parse chat response from agent {self.name}."
             ) from e
 
     async def execute_task(
         self, name: str, description: str, assignment: TaskAssignment
     ) -> None:
-        try:
-            response = await self._request(
-                "POST",
-                f"{self.url}/tasks",
-                json={
-                    "task": name,
-                    "description": description,
-                    "assignment": assignment.dict(),
-                },
-            )
-            assert response.status == 200
-        except AssertionError as e:
-            raise errors.TaskError(
-                f"Could not execute task {name} on agent {self.name}."
-            ) from e
+        await self._request(
+            "POST",
+            f"{self.url}/tasks",
+            json={
+                "task": name,
+                "description": description,
+                "assignment": assignment.dict(),
+            },
+        )
 
     async def update_task(self, name: str, description: str) -> None:
-        try:
-            response = await self._request(
-                "PATCH",
-                f"{self.url}/tasks/{name}",
-                json={"name": name, "description": description},
-            )
-            assert response.status == 200
-        except AssertionError as e:
-            raise errors.TaskError(
-                f"Could not update task {name} on agent {self.name}."
-            ) from e
+        await self._request(
+            "PATCH",
+            f"{self.url}/tasks/{name}",
+            json={"name": name, "description": description},
+        )
 
     async def list_tasks(self) -> list[TaskStatus]:
         try:
-            response = await self._request("GET", f"{self.url}/tasks")
-            assert response.status == 200
-            return [TaskStatus(**task_status) for task_status in await response.json()]
-        except AssertionError as e:
-            raise errors.TaskError(
-                f"Could not fetch task status from agent {self.name}."
-            ) from e
-        except (pydantic.ValidationError, aiohttp.ContentTypeError) as e:
+            response_data = await self._request("GET", f"{self.url}/tasks")
+            return [TaskStatus(**task_status) for task_status in response_data]
+        except (TypeError, pydantic.ValidationError) as e:
             raise errors.TaskError(
                 f"Could not parse response from agent {self.name}."
             ) from e
 
     async def get_task(self, task: str) -> TaskStatus:
         try:
-            response = await self._request("GET", f"{self.url}/tasks/{task}")
-            assert response.status == 200
-            return TaskStatus(**await response.json())
-        except AssertionError as e:
-            raise errors.TaskError(
-                f"Could not fetch task status from agent {self.name}."
-            ) from e
-        except (pydantic.ValidationError, aiohttp.ContentTypeError) as e:
+            response_data = await self._request("GET", f"{self.url}/tasks/{task}")
+            return TaskStatus(**response_data)
+        except pydantic.ValidationError as e:
             raise errors.TaskError(
                 f"Could not parse response from agent {self.name}."
             ) from e
 
     async def cancel_task(self, task: str) -> None:
-        try:
-            response = await self._request("DELETE", f"{self.url}/tasks/{task}")
-            assert response.status == 200
-        except AssertionError as e:
-            raise errors.TaskError(
-                f"Could not cancel task {task} on agent {self.name}."
-            ) from e
+        await self._request("DELETE", f"{self.url}/tasks/{task}")
